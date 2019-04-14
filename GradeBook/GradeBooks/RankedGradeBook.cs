b2cd55dc26e20f0faec0e6f0020cdf140b87c035
Rabikauskas, Algirdas <algirdas.rabikauskas@gmail.com>
2019-04-14 14:29:38 +0300

    Second 20 Percent Get a B

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,10 +15,15 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
             var countOfStudentsWithHigherAverageGrade = Students.Count(s => s.AverageGrade > averageGrade);
-            double percentOfHigherAverageGrade = countOfStudentsWithHigherAverageGrade / Students.Count;
+            var percentOfHigherAverageGrade = (double)countOfStudentsWithHigherAverageGrade / (double)Students.Count;
             if (percentOfHigherAverageGrade < 0.2)
             {
                 return 'A';
+            }
+
+            if (percentOfHigherAverageGrade < 0.4)
+            {
+                return 'B';
             }
             return 'F';
         }
